e66adc9371d7697b741c8218ac1a4a23a7832183
Pravesh001 <praveshraykwar@gmail.com>
2021-05-31 17:00:44 +0530

    commiting all files

---
+++ upload.py
@@ -0,0 +1,39 @@
+import os
+import boto3
+import csv
+from flask import *  
+
+PEOPLE_FOLDER = os.path.join('static', 'people_photo')
+
+app = Flask(__name__)  
+app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
+
+#readinng aws keys
+with open('pravesh_api_new_user_credentials.csv', 'r') as input:
+    next(input) #goto second line
+    reader = csv.reader(input)
+    for line in reader:
+        access_key_id = line[2] #see file .csv
+        secret_access_key = line[3]
+
+
+
+@app.route('/')  
+def upload():  
+    return render_template("file_upload_form.html")  
+     
+@app.route('/success', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
+        f = request.files['file']  
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))  
+        file_n = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
+        client = boto3.client ('rekognition', aws_access_key_id = access_key_id, aws_secret_access_key = secret_access_key)
+        photo = file_n
+        with open(photo,'rb') as source_image:
+            source_bytes = source_image.read() #get photo in bytes
+        response = client.detect_faces(Image = {'Bytes': source_bytes},Attributes = ['ALL'])
+        return render_template("success.html",user_image = file_n, name = response )  
+      
+if __name__ == '__main__':  
+    app.run(debug = True)  
